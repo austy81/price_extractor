0b50fd0a7e896b93890b2f2ac85f0b0eeefa5540
Honza Austerlitz <janau@jaauster1.europe.corp.microsoft.com>
2018-03-31 23:00:52 +0200

    Cleaning price fix decimal

--- extract_price.py
+++ extract_price.py
@@ -2,13 +2,18 @@ import re
 
 
 def clean(text):
-    if not text: 
+    if not text:
         return None
     try:
         text = text.decode('utf-8')
     except:
         pass
     numbers = re.sub('[^0-9,\.]+', '', text).replace(',', '.')
+
+    by_dot = numbers.split('.')
+    if len(by_dot) > 1 and len(by_dot[1]) == 3:
+        numbers = numbers.replace('.', '', len(by_dot)-2 if len(by_dot) > 2 else 1)
     if len(numbers) == 0:
         return None
+
     return float(numbers)
